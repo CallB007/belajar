566ce3ab690f7cdb92905b17ab38f3258766bfd2
CallB007 <josephwijaya80@gmail.com>
2023-04-18 22:50:03 +0800

    belajar dart dasar

---
+++ bin/operator_perbandingan.dart
@@ -0,0 +1,13 @@
+void main (){
+
+  print(10 == 10);
+  print(10 != 10);
+  print(10 > 10);
+  print(10 < 10);
+  print(10 >= 10);
+  print(10 <= 10);
+
+  print('joseph' == 'joseph');
+  print('joseph' != 'joseph');
+
+}
