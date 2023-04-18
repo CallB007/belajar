a2bdc0c5ee80d89452c6b0ac1cbbbb0e258f73a7
CallB007 <josephwijaya80@gmail.com>
2023-04-18 22:34:22 +0800

    belajar dart dasar

---
+++ bin/conversion_boolean.dart
@@ -0,0 +1,13 @@
+void main (){
+
+  var inputString = 'true';
+  ///Mengubah String ke Bolean
+  var inputBoolean = inputString =='true';
+
+  ///Mengubah Boolean ke String
+  var booleaToString = inputBoolean.toString();
+
+  print(inputBoolean);
+  print(booleaToString);
+
+}
